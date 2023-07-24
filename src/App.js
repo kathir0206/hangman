1c2289bab4411d006616a97d17b80ef41d5b636f
kathir0206 <110644429+kathir0206@users.noreply.github.com>
2023-07-24 19:48:21 +0530

    Add files via upload
    src and public folder along with rest of the JSON are uploaded

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Hangman from './Components/Hangman'
+
+function App() {
+  return (
+    <div className="App">
+      <Hangman/>
+    </div>
+  );
+}
+
+export default App;
